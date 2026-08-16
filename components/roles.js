4d5a29caf246a483e6269c0474f76f4aa06afa30
Vexol-files <wolskidawid07@gmail.com>
2026-08-16 15:05:21 +0200

    Create roles.js

---
+++ components/roles.js
@@ -0,0 +1,14 @@
+export const ROLE_DEFS = {
+  "1532749280220024842": { name: "Premium", color: "#FFD700", hidden: false },
+  "1536693194744660039": { name: "Beta", color: "#C060FF", hidden: false },
+  "1534495312377417868": { name: "Booster", color: "#8A2BE2", hidden: false },
+
+  "1534495410566201427": { name: "Trusted", color: "#FFCCCC", hidden: true },
+  "1532716147093274685": { name: "Moderator", color: "#4FC3F7", hidden: true },
+  "1536692816284094554": { name: "Admin", color: "#FF6666", hidden: true },
+  "1536692884928208936": { name: "HeadAdmin", color: "#FF0000", hidden: true },
+  "1532711838264066129": { name: "Co-Owner", color: "#FF0000", hidden: true },
+
+  "1529827656428818492": { name: "Owner", color: "#FF0000", hidden: true },        // Owner człowiek
+  "1534494523768569897": { name: "Owner Bot", color: "#8B0000", hidden: true }     // Owner bot
+};
