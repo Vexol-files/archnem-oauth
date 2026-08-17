27806a21264b2c1ed66b92fb04bb056dfe039392
Vexol-files <wolskidawid07@gmail.com>
2026-08-17 10:34:58 +0200

    Update logout.js

--- api/logout.js
+++ api/logout.js
@@ -1,6 +1,8 @@
 // /api/logout.js
 export default function handler(req, res) {
-  // Usuń cookie sesyjne i przekieruj na stronę główną
-  res.setHeader("Set-Cookie", "archnem_token1=; HttpOnly; Secure; SameSite=None; Path=/; Max-Age=0");
+  res.setHeader("Set-Cookie", [
+    "token=; HttpOnly; Secure; SameSite=None; Path=/; Max-Age=0",
+    "archnem_oauth_state=; HttpOnly; Secure; SameSite=None; Path=/; Max-Age=0"
+  ]);
   return res.redirect("https://archnem.xo.je");
 }
